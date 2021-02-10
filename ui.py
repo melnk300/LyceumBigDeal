de2f4965400291b7a27420fadd1b07b55c9a6db4
WickedBun <74819327+WickedBun@users.noreply.github.com>
2021-02-10 16:38:16 +0500

    Create ui.py

---
+++ ui.py
@@ -0,0 +1,53 @@
+import os
+import sys
+
+import requests
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+
+SCREEN_SIZE = [600, 450]
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.getImage()
+        self.initUI()
+
+    def getImage(self):
+        delta = "0.002"
+        ll = "37.530887,55.703118"
+        geocoder_api_server = "http://static-maps.yandex.ru/1.x/"
+        geocoder_params = {
+            "ll": ll,
+            "spn": ",".join([delta, delta]),
+            "l": "map",
+        }
+
+        response = requests.get(geocoder_api_server, params=geocoder_params)
+        print(response.url)
+
+        self.map_file = "map.png"
+        with open(self.map_file, "wb") as file:
+            file.write(response.content)
+
+    def initUI(self):
+        self.setGeometry(100, 100, *SCREEN_SIZE)
+        self.setWindowTitle('Отображение карты')
+
+        self.pixmap = QPixmap(self.map_file)
+        self.image = QLabel(self)
+        self.image.move(0, 0)
+        self.image.resize(600, 450)
+        self.image.setPixmap(self.pixmap)
+
+    def closeEvent(self, event):
+        """При закрытии формы подчищаем за собой"""
+        os.remove(self.map_file)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())

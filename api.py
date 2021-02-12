cebade1d76d2bf2e52e3902a0c422120e8f217cb
Ilya <wickedbun@yandex.ru>
2021-02-12 19:46:53 +0500

    Изменение масштаба

--- api.py
+++ api.py
@@ -2,11 +2,10 @@ import requests as rq
 
 static_address = "http://static-maps.yandex.ru/1.x/"
 geocode_address = "http://geocode.yandex.ru/1.x/"
-spn = 0.05
 
 
-def get_img(coords, spn='0.01,0.01'):  # => coords = (lon - долгота lat - широта)
-    res = rq.get("http://static-maps.yandex.ru/1.x/", {'ll': coords, 'spn': spn, 'l': 'map'})
-    with open('map.png', 'ab+') as f:
+def get_img(coords, z='14'):  # => coords = (lon - долгота lat - широта)
+    res = rq.get("http://static-maps.yandex.ru/1.x/", {'ll': coords, 'z': z, 'l': 'map'})
+    with open('map.png', 'wb+') as f:
         f.write(res.content)
     return res
--- ui.py
+++ ui.py
@@ -1,55 +1,41 @@
 import os
 import sys
-
-import requests
 import api
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
-from PyQt5 import QtCore
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5 import QtCore, uic
 
 SCREEN_SIZE = [600, 450]
+
+
 class Example(QWidget):
     def __init__(self):
         super().__init__()
-        self.getImage()
-        self.initUI()
-        self.spn = 0.05
-
-    def initUI(self):
-        self.setGeometry(100, 100, *SCREEN_SIZE)
+        uic.loadUi('ui.ui', self)
         self.setWindowTitle('Отображение карты')
-        self.spn = 0.05
-        self.pixmap = QPixmap(self.map_file)
-        self.image = QLabel(self)
-        self.image.move(0, 0)
-        self.image.resize(600, 450)
-        self.image.setPixmap(self.pixmap)
+
+        self.z = 14
+        self.coords = '76.564993,60.938296'
+        self.map_file = "map.png"
+        self.getImage()
 
     def getImage(self):
-        res = api.get_img('76.564993,60.938296', spn=','.join(list(map(str, [self.spn, self.spn]))))
-        print(res.url)
-        self.map_file = "map.png"
-        self.repaint()
+        res = api.get_img(self.coords, str(self.z))
+        self.pixmap = QPixmap(self.map_file)
+        self.image.setPixmap(self.pixmap)
+        # print(res.url)
 
     def closeEvent(self, event):
         """При закрытии формы подчищаем за собой"""
         os.remove(self.map_file)
 
-    def pqup(self):
-        spn += 0.01
-        print(spn)
-        self.getImage()
-
-    def pqdown(self):
-        spn -= 0.01
-        print(spn)
-        self.getImage()
-
     def keyPressEvent(self, e):
-        if e.key() == QtCore.Qt.Key_Up:
-            self.pqup()
-        elif e.key() == QtCore.Qt.Key_Down:
-            self.pqdown()
+        if e.key() == QtCore.Qt.Key_PageUp:
+            self.z += 1
+            self.getImage()
+        elif e.key() == QtCore.Qt.Key_PageDown:
+            self.z -= 1
+            self.getImage()
 
 
 if __name__ == '__main__':
